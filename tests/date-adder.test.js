db74d218edee3fba0d25940e3985e685fb097fc7
allisonbusse <allisonbusse@gmail.com>
2019-10-21 20:02:32 -0700

    finished tests

--- tests/date-adder.test.js
+++ tests/date-adder.test.js
@@ -23,11 +23,32 @@ describe('Date Adder Function', () => {
     expect(result).toEqual(new Date(1986, 7, 17, 22, 33, 22, 45));
   });
 
-  it('can days to a date', () => {
+  it('can add days to a date', () => {
     const newDate = new Date(1986, 7, 17, 12, 33, 22, 45);
     const diffTest = '10d';
     const result = dateAdder(newDate, diffTest);
     expect(result).toEqual(new Date(1986, 7, 27, 12, 33, 22, 45));
+  });
+
+  it('can add weeks to a date', () => {
+    const newDate = new Date(1986, 7, 17, 12, 33, 22, 45);
+    const diffTest = '10w';
+    const result = dateAdder(newDate, diffTest);
+    expect(result).toEqual(new Date(1986, 9, 26, 12, 33, 22, 45));
+  });
+
+  it('can add months to a date', () => {
+    const newDate = new Date(1986, 7, 17, 12, 33, 22, 45);
+    const diffTest = '10M';
+    const result = dateAdder(newDate, diffTest);
+    expect(result).toEqual(new Date(1987, 5, 17, 12, 33, 22, 45));
+  });
+
+  it('can add years to a date', () => {
+    const newDate = new Date(1986, 7, 17, 12, 33, 22, 45);
+    const diffTest = '10y';
+    const result = dateAdder(newDate, diffTest);
+    expect(result).toEqual(new Date(1996, 7, 17, 12, 33, 22, 45));
   });
 
 
